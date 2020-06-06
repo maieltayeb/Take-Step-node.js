e68ee486517deb4212be5640d37bcb3e44f20d8f
hala <HalaNazmy12@gmail.com>
2020-06-06 21:15:35 +0200

    add skill

--- models/Skill.js
+++ models/Skill.js
@@ -1,21 +1,18 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 // const _ = require('lodash');
 // const validator=require('validator');
 
-
-const SkillSchema=new mongoose.Schema({
-    SkillName:{
-     type: String,
-     required:true,
-     unique: true,
- 
+const SkillSchema = new mongoose.Schema(
+  {
+    SkillName: {
+      type: String,
+      required: true,
+      unique: true
     }
-   
+  },
+  {}
+);
 
-},{});
+const Skill = mongoose.model("Skill", SkillSchema);
 
-
-const Skill=mongoose.model('Skill',SkillSchema);
-
-
-module.exports=Skill;
+module.exports = Skill;
